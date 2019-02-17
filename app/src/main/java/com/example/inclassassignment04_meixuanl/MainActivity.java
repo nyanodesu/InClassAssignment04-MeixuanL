ff9b3b127e6cd0879d771b0e6697751700c86a14
nyanodesu <ml4199@tc.columbia.edu>
2019-02-16 23:35:46 -0500

    Initial commit

---
+++ app/src/main/java/com/example/inclassassignment04_meixuanl/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.inclassassignment04_meixuanl;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText mEditText;
+    float textSize = 12;
+    TextView mTextView;
+    String text;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mEditText = (EditText) findViewById(R.id.input_text_view);
+        mTextView = (TextView) findViewById(R.id.text);
+
+    }
+
+    public void copyText(View v){
+        text =  mEditText.getText().toString();
+        mTextView.setText(text);
+        mTextView.setTextSize(textSize);
+    }
+
+    public void doubleText(View v){
+        text = text + text;
+        mTextView.setText(text);
+        mTextView.setTextSize(textSize);
+    }
+
+    public void upText(View v){
+        textSize = textSize + 1;
+        mTextView.setTextSize(textSize);
+    }
+
+    public void downSize(View v){
+        textSize = textSize - 1;
+        mTextView.setTextSize(textSize);
+    }
+
+    @Override
+    protected void onResume(){
+        Toast.makeText(MainActivity.this,"Welcome!",Toast.LENGTH_SHORT).show();
+        super.onResume();
+    }
+
+}
